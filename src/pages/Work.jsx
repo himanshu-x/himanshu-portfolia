568e43b3e5cb22ceaec38f472d25af6e78afa9e3
himanshu-x <hy12632@gmail.com>
2022-09-14 22:08:14 +0530

    scroll add i work file

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Veganfood from '../assets/veganfood.png';
 import DumyFoodWeb from '../assets/dumyfood.png';
+import Tours from '../assets/tours.png';
 
 const Work = () => {
     return (
@@ -17,9 +18,10 @@ const Work = () => {
                 <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 min-h-[300px]'>
 
                     {/* Grid Item */}
-                    <div
+                    <marquee behavior="scroll" direction="up"
                         style={{ backgroundImage: `url(${Veganfood})` }}
-                        className=' bg-cover  shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '
+                        className=' bg-cover 
+                        object-center  shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '
                     >
                         {/* Hover Effects */}
                         <div className='opacity-0 group-hover:opacity-100 mt-6 '>
@@ -39,8 +41,8 @@ const Work = () => {
                                 </a>
                             </div>
                         </div>
-                    </div>
-                    <div
+                    </marquee>
+                    <marquee behavior="scroll" direction="up"
                         style={{ backgroundImage: `url(${DumyFoodWeb})` }}
                         className=' bg-cover shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
                     >
@@ -62,7 +64,30 @@ const Work = () => {
                                 </a>
                             </div>
                         </div>
-                    </div>
+                    </marquee>
+                    <marquee
+                        behavior="scroll" direction="up" style={{ backgroundImage: `url(${Tours})` }}
+                        className=' bg-cover  shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+                    >
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100 mt-6'>
+                            <span className='text-2xl font-bold text-white tracking-wider '>
+                                React JS Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href='/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Demo
+                                    </button>
+                                </a>
+                                <a href='/'>
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+                    </marquee>
                 </div>
             </div>
         </div>
