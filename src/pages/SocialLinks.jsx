3e7ea5fbe607f250f20c5d7ab523f235a7839bf9
himanshu-x <hy12632@gmail.com>
2022-10-17 14:30:30 +0530

    use  noreferrer

--- src/pages/SocialLinks.jsx
+++ src/pages/SocialLinks.jsx
@@ -7,10 +7,10 @@ export default function SocialLinks() {
     return (
 
         <div className="flex lg:hidden gap-4 justify-center items-center text-white   bg-[#0a192f] min-h-[200px]">
-            <a target="_blank" href="https://www.linkedin.com/in/himanshu1807/"><FaLinkedin size={40} /></a>
-            <a target="_blank" href="https://github.com/himanshu-x"> <FaGithub size={40} /></a>
-            <a target="_blank" href="https://twitter.com/himanshu87694"><FaTwitterSquare size={40} /></a>
-            <a target="_blank" href="https://www.facebook.com/itshimanshu1807/"><BsFacebook size={40} /></a>
+            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/himanshu1807/"><FaLinkedin size={40} /></a>
+            <a target="_blank" rel="noreferrer" href="https://github.com/himanshu-x"> <FaGithub size={40} /></a>
+            <a target="_blank" rel="noreferrer" href="https://twitter.com/himanshu87694"><FaTwitterSquare size={40} /></a>
+            <a target="_blank" rel="noreferrer" href="https://www.facebook.com/itshimanshu1807/"><BsFacebook size={40} /></a>
         </div>
 
     )
